8a0968cf220d70baab8a53c97a9ae4e6187f7f75
Doniyor11 <udoniyorbek99@mail.ru>
2025-09-29 17:45:41 +0500

    29 09 25

--- src/widgets/layouts/navbar/index.tsx
+++ src/widgets/layouts/navbar/index.tsx
@@ -1,8 +1,10 @@
 import {
   ActionIcon,
   Burger,
+  Button,
   Collapse,
   Drawer,
+  Flex,
   Menu,
   Select,
   Text,
@@ -17,7 +19,8 @@ import { MenuData } from "@/widgets/layouts/navbar/libs.ts"
 
 import IconDown from "@/shared/assets/images/icons/icon-chevron-down.svg"
 import IconClose from "@/shared/assets/images/icons/icon-close.svg"
-import IconGlobal from "@/shared/assets/images/icons/icon-global.svg"
+import IconGlobal from "@/shared/assets/images/interise-group/language.svg"
+import IconLogo from "@/shared/assets/images/interise-group/logo.svg"
 
 import s from "./styles.module.scss"
 
@@ -30,7 +33,7 @@ export const Navbar = () => {
     <>
       <div className={cx(s.navbarWrapper, "container")}>
         <Link href={"/"} className={s.logo}>
-          Chustiy
+          <IconLogo />
         </Link>
 
         <ul className={s.menu}>
@@ -81,12 +84,26 @@ export const Navbar = () => {
           })}
         </ul>
 
-        <Select
-          className={s.lang}
-          defaultValue={"Uz"}
-          data={["Ru", "Uz"]}
-          leftSection={<IconGlobal />}
-        />
+        {/*<Select*/}
+        {/*  className={s.lang}*/}
+        {/*  defaultValue={"Uz"}*/}
+        {/*  data={["Ru", "Uz"]}*/}
+        {/*  leftSection={<IconGlobal />}*/}
+        {/*/>*/}
+        <Flex gap={5} align={"center"}>
+          <Button
+            leftSection={<IconGlobal />}
+            bg={"#2C2C2C"}
+            color={"#FFF"}
+            radius={8}
+            h={"44px"}
+          >
+            RU/ENG
+          </Button>
+          <Button bg={"#0076FE"} color={"#FFF"} radius={8} h={"44px"}>
+            Связаться с нами
+          </Button>
+        </Flex>
         {matches && (
           <Burger opened={isOpen} onClick={() => setIsOpen(!isOpen)} />
         )}
--- src/widgets/layouts/navbar/libs.ts
+++ src/widgets/layouts/navbar/libs.ts
@@ -1,64 +1,79 @@
 export const MenuData = [
   {
-    text: "Asosiy",
+    text: "О нас",
     path: "/",
     children: [],
   },
   {
-    text: "Biz haqimizda",
-    path: "/about",
+    text: "Направления работы",
+    path: "/",
     children: [],
   },
   {
-    text: "Xizmatlar",
-    path: "services",
-    children: [
-      {
-        text: "Katering",
-        path: "/services/catering",
-      },
-      {
-        text: "Konsalting",
-        path: "/services/consulting",
-      },
-    ],
-  },
-  {
-    text: "Restoranlar",
-    path: "/restaurants",
+    text: "Исследования",
+    path: "/",
     children: [],
   },
   {
-    text: "Loyihalar",
-    path: "projects",
-    children: [
-      {
-        text: "Do’konlar",
-        path: "/projects/shops",
-      },
-      {
-        text: "Kitoblar",
-        path: "/projects/books",
-      },
-      {
-        text: "Retseptlar",
-        path: "/projects/recipes",
-      },
-    ],
-  },
-  {
-    text: "Tadbirlar",
-    path: "events",
-    children: [
-      {
-        text: "Festivallar",
-        path: "/events/festivals",
-      },
-    ],
-  },
-  {
-    text: "Kontaktlar",
-    path: "/contacts",
+    text: "Interise Club",
+    path: "/",
     children: [],
   },
+  {
+    text: "Контакты",
+    path: "/",
+    children: [],
+  },
+  // {
+  //   text: "Xizmatlar",
+  //   path: "services",
+  //   children: [
+  //     {
+  //       text: "Katering",
+  //       path: "/services/catering",
+  //     },
+  //     {
+  //       text: "Konsalting",
+  //       path: "/services/consulting",
+  //     },
+  //   ],
+  // },
+  // {
+  //   text: "Restoranlar",
+  //   path: "/restaurants",
+  //   children: [],
+  // },
+  // {
+  //   text: "Loyihalar",
+  //   path: "projects",
+  //   children: [
+  //     {
+  //       text: "Do’konlar",
+  //       path: "/projects/shops",
+  //     },
+  //     {
+  //       text: "Kitoblar",
+  //       path: "/projects/books",
+  //     },
+  //     {
+  //       text: "Retseptlar",
+  //       path: "/projects/recipes",
+  //     },
+  //   ],
+  // },
+  // {
+  //   text: "Tadbirlar",
+  //   path: "events",
+  //   children: [
+  //     {
+  //       text: "Festivallar",
+  //       path: "/events/festivals",
+  //     },
+  //   ],
+  // },
+  // {
+  //   text: "Kontaktlar",
+  //   path: "/contacts",
+  //   children: [],
+  // },
 ]
